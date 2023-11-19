ef3424caa9d91dcb3a93c0cda68f32c3d7407b31
Qisthi <145886901+Qisthi26@users.noreply.github.com>
2023-11-19 20:01:36 +0700

    Add files via upload

---
+++ VariabelNama.java
@@ -0,0 +1,23 @@
+
+package VariabelNama;
+
+public class VariabelNama {
+    public static void main(String[] args) {
+        //membuat variabel
+        int umur;
+        String kelas, nama, alamat;
+        
+        //mengisi variabel
+        nama = "Muna Aliya Bil Qisthi";
+        alamat = "Tangerang 11";
+        kelas = "X Tel 09";
+        umur = 16;
+        
+        //mencetak ke layar isi variabel
+        System.out.println("nama = "+nama);
+        System.out.println("alamat = "+alamat);
+        System.out.println("kelas = "+kelas);
+        System.out.println("umur = "+umur+ " tahun");
+        
+    }
+}
