1709dc4f71eecd16ad7a616de8a06b114843b8cc
Qisthi <145886901+Qisthi26@users.noreply.github.com>
2023-11-19 20:01:15 +0700

    Add files via upload

---
+++ VariabelMTK.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package VariabelMTK;
+
+/**
+ *
+ * @author munaa
+ */
+public class VariabelMTK {
+    public static void main(String[]args){
+        
+        System.out.println("Hello Telkom");
+        System.out.println("Apa kabar?");
+        
+        var x = 5;
+        var y = 10;
+        var z = x + y; //penjumlahan variabel x dan y
+        System.out.println(z); //menampilkan penjumlahan variabel z
+        
+        System.out.println("Mencari luas segitiga");
+        System.out.println("Rumus luas segitiga : 1/2*alas*tinggi");
+        System.out.println("Alas:4");
+        System.out.println("Tinggi:5");
+        
+        var a = 4;
+        var t = 5;
+        var b = a * t / 2;
+        System.out.println("Luas Segitiga:" + b + "cm2");
+        
+        
+        //Buat program mencari keliling lingkaran
+        
+        System.out.println("Mencari Keliling lingkaran");
+        System.out.println("Rumus Keliling lingkaran phi*(r*r)");
+        System.out.println("phi: 22/7");
+        System.out.println("r: 21");
+        
+        var phi = 22/7;
+        var r = 21;
+        var kl = phi * (r*r);
+        System. out.println("Keliling lingkaran =" + kl + "cm");
+        
+    }
+}
+//operator arimatika
+//perkalian, pembagian, penjumlahan, penambahan, pemangkatan, modulus
+//*, /, +, -, **, %
+
+//String = text
+//integer = angka
