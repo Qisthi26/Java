c5e783300320cd589859740fb5d1a69abf385f20
Qisthi26 <145886901+Qisthi26@users.noreply.github.com>
2023-11-19 19:18:55 +0700

    Add files via upload

---
+++ PboX.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pbox;
+
+/**
+ *
+ * @author munaa
+ */
+public class PboX {
+
+    public static void main(String[] args) {
+        System.out.println("Muna Aliya Bil Qisthi - X Tel 9");
+        System.out.println("SMK Telkom Jakarta");
+    }
+}
