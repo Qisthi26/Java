7802b365ebbaeee623c004f8984cbe5b9e6bde41
Qisthi <145886901+Qisthi26@users.noreply.github.com>
2023-11-19 19:52:18 +0700

    Add files via upload

---
+++ Hello_World.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pbox;
+
+/**
+ *
+ * @author munaa
+ */
+public class Hello_World {
+
+    public static void main(String[] args) {
+        System.out.println("Muna Aliya Bil Qisthi - X Tel 9");
+        System.out.println("SMK Telkom Jakarta");
+    }
+}
